340238679940e48b2f497feffd79820f6fd35f93
jmponcem <josemiguelponcem@gmail.com>
2022-09-08 13:45:04 -0400

    proyecto java spring

---
+++ src/main/resources/static/js/registrar.js
@@ -0,0 +1,29 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+});
+
+async function registrarUsuario(){
+    let datos = {};
+    datos.nombre = document.getElementById('txtNombre').value;
+    datos.apellido = document.getElementById('txtApellido').value;
+    datos.email = document.getElementById('txtEmail').value;
+    datos.password = document.getElementById('txtContraseña').value;
+
+    let repetirPassword = document.getElementById('txtRepetirContraseña').value;
+    
+    if(repetirPassword !=datos.password){
+        alert('La contraseña que escribiste es diferente.');
+        return;
+    }
+ const request = await fetch('api/usuarios',{
+    method: 'POST',
+    headers:{
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(datos)
+ });
+    alert("La cuenta fue creada con exito!.");
+    window.location.href = 'login.html'
+}
+
